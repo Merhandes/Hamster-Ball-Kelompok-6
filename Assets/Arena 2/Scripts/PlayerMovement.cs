0669550268247fbe62723ff94b0629546a50b80e
genaduttt <ggenaduttt@gmail.com>
2024-12-13 16:09:42 +0700

    nambah poin dan perbaiki ui

--- Assets/Arena 2/Scripts/PlayerMovement.cs
+++ Assets/Arena 2/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Tambahkan namespace TextMeshPro
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,21 +10,31 @@ public class PlayerMovement : MonoBehaviour
     private float xInput;
     private float zInput;
 
+    // Ganti tipe Text menjadi TextMeshProUGUI
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+    private int count;
 
     // Update is called once per frame
     void Update()
     {
         Inputs();
     }
+
     private void FixedUpdate()
     {
         Move();
     }
+
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        count = 0; // Inisialisasi count
+        SetCountText(); // Perbarui UI di awal
+        winText.text = ""; // Kosongkan teks kemenangan di awal
     }
+
     private void Inputs()
     {
         xInput = Input.GetAxis("Horizontal");
@@ -32,7 +43,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        rigid.AddForce(new Vector3(xInput, 0f, zInput)*speed);
-    } 
+        rigid.AddForce(new Vector3(xInput, 0f, zInput) * speed);
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
