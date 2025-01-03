a382037b72df620f38936370ded381cde15536bf
CODE JFK <farreliuskevin@gmail.com>
2025-01-03 22:22:33 +0700

    Berhasil Perbaikin kesalahan push

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    public Slider sliderMusic;
     public AudioSource sumberSuara;
+    public float volume = 1;
 
-    public void ketikaSliderDiubah(float nilaiSlider)
+    public const string keyVolume = "VOLUME";
+
+    private void Start()
     {
-        sumberSuara.volume = nilaiSlider;
+        float volumeTerakhir = PlayerPrefs.GetFloat(keyVolume);
+        KetikaSliderDiubah(volumeTerakhir);
+        sliderMusic.value = volumeTerakhir;
     }
-    
+
+    public void KetikaSliderDiubah(float nilaiSlider)
+    {
+        volume = nilaiSlider;
+        sumberSuara.volume = nilaiSlider;
+
+        PlayerPrefs.SetFloat(keyVolume, nilaiSlider);
+    }
 }
